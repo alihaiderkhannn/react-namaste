bc588b85413adc32409760c9d28cc6ebb231f52e
Ali Haider Khan <ali_786@live.no>
2025-02-10 13:59:27 +0100

    playing more to understand more of the fundamental in react

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
-const heading = React.createElement(
-"h1",
-{className: "heading"},
-"Hello World from React!");
+/**
+ * 
+ * <div id="parent">
+ *    <div id="child>
+ *   <h1>I'm h1 tag</h1>
+ *   <h2>I'm h2 tag</h2>
+ *   </div>
+ * <div id ="child2">
+ *  <h1>I'm h1 tag</h1>
+ *  <h2>I'm h2 tag</h2>
+ * </div>
+ * </div>
+ * 
+ * 
+ */
 
-// the above are the props. 
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    [
+        React.createElement("div", { id: "child"}, [
+        React.createElement("h1", {}, "I'm h1 tag"),
+        React.createElement("h2", {}, "I'm h2 tag")
+        ]),
+        React.createElement("div", { id: "child2"}, [
+        React.createElement("h1", {}, "I'm h1 tag"),
+        React.createElement("h2", {}, "I'm h2 tag")
+        ])
+    ]
 
-console.log(heading); //this returns a js object. a react element.
-        
+);
+
+console.log(parent);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
 
-root.render(heading);
+// const heading = React.createElement
+// ("h1", 
+// {className: "heading"},
+// "Hello World from React");
+
+// console.log(heading);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));  
+// root.render(heading);
