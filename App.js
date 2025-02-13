cef710574d78ed27842b754ed4986376ec7ab25f
Ali Haider Khan <ali_786@live.no>
2025-02-13 22:01:51 +0100

    ignited our app

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 /**
  * 
  * <div id="parent">
@@ -19,7 +22,7 @@ const parent = React.createElement(
     { id: "parent" },
     [
         React.createElement("div", { id: "child"}, [
-        React.createElement("h1", {}, "I'm h1 tag"),
+        React.createElement("h1", {}, "This is Namaste React"),
         React.createElement("h2", {}, "I'm h2 tag")
         ]),
         React.createElement("div", { id: "child2"}, [
