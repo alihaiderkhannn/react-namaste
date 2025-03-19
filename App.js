c41292f548609ae34a396ab9ca7462772d07327b
Ali Haider Khan <ali_786@live.no>
2025-03-19 15:05:53 +0100

    working with the food app

--- App.js
+++ App.js
@@ -1,61 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style.css'
+import './index.css';
 import logo from './images/logo.png'
 
 
-// const nestedHeader = React.createElement('div', { class: "title" },
-//     [
-//         React.createElement('h1', {} , "This is an H1 tag"), 
-//         React.createElement('h2', {} , "This is an H2 tag"),
-//         React.createElement('h3', {} , "This is an H3 tag"),
-//     ]
-// )
-// const nestedHeaderUsingJSX = (
-//     <div className='title'>
-//         <h1>This is an h1 tag</h1>
-//         <h2>This is an h2 tag</h2>
-//         <h3>This is an h3 tag</h3>
-//     </div>
-// )
+const HeaderComponent = () => {
+    return (
+    <div className='header'>
+        <div className='logo'>
+            <img src='https://th.bing.com/th/id/OIP.9hl54qFeHA_2o_PIo3JhswAAAA?rs=1&pid=ImgDetMain' />
+        </div>
+        <div className='nav-items'>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+    </div>
+    )
+}
 
-// const AliComponent = () => {
-//     return (
-//         <div className='alis-component'>
-//             <h1>
-//                 My name is Ali
-//             </h1>
+const BodyComponent = () => {
 
-//         </div>
-//     )
-// }
+}
 
-// const HeaderComponent = () => {
-//     return (
-//         <div className='title'>
-//             <AliComponent />
-//             <h1 id='heading-1' style={{ color: 'red'}}>This is an h1 tag</h1>
-//             <h2 id='heading-2' style={{ fontSize: "50px"}}>This is an h2 tag</h2>
-//             <h3 id='heading-3' style={{ fontFamily: "sans-serif"}}>This is an h3 tag</h3>
-//         </div>
-//     )
-// }
+const AppLayout = () => {
+    return (
+        <div className='app'>
+            <HeaderComponent />
+            <BodyComponent />
+        </div>
+    )
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
-// const Header = () => {
-//     return (
-//       <header className='header'>
-//         <div className='logo'>
-//             <img src={logo} alt='united logo'></img>
-//         </div>
-//         <div className='search-bar'>
-//             <input type='text' placeholder='Search...'></input>
-//         </div>
-//       </header>
-
-//     )
-// }
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<Header />);
+root.render(<AppLayout />);
 
